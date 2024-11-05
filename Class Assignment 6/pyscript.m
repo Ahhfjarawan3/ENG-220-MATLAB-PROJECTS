0552961df4005aee3338f4fe83394e7d800ec7e5
Ahhfjarawan3 <ahhfjarawan@unm.edu>
2024-11-05 23:36:34 +0200

    Update pyscript.m

--- Class Assignment 6/pyscript.m
+++ Class Assignment 6/pyscript.m
@@ -1,11 +1,18 @@
+%Configure Python in MATLAB
+pyversion('C:/path/to/python.exe');
+
+%Calling Python Functions from MATLAB
 mylist = py.list({'apple', 'banana', 'orange'});
 len_of_list = py.len(mylist);
 disp(len_of_list);
 
+%Using Python Libraries in MATLAB
 py_math = py.importlib.import_module('math');
 result = py_math.sqrt(25);
 disp(result);
 
-py_script = py.importlib.import_module('greet');
-greeting = py_script.greet('Abdullah Jarawan');
-disp(greeting);
+%Running Custom Python Scripts
+reverse_module = py.importlib.import_module('reverse_string');
+my_str = 'Hello MATLAB';
+reversed_str = reverse_module.reverse_string(my_str);
+disp(reversed_str);
